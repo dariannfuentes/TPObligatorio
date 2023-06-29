2bd60912811c2f32a0903a43cf1d6ac1844a729c
dariannfuentes <dariannfuentes@gmail.com>
2023-06-29 11:28:07 -0300

    Cree la carpeta models y declare los objetos

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent {
-
+export class ItemsComponent implements OnInit{
+  items: Item[];
 }
---
+++ src/app/models/item.ts
@@ -0,0 +1,9 @@
+import { NumberValueAccessor } from "@angular/forms";
+
+export class Item {
+    id: number = 0;
+    title: string = '';
+    price: number = 0;
+    quantity: number = 0;
+    completed: boolean = false;
+}
